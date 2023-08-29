38621466296ca9bc8d96e7e0d89dd34da1580dca
junam2 <speedlin4584@gmail.com>
2023-08-29 22:43:41 +0900

    Initial commit

---
+++ src/main/java/com/junam/my_bucket_server/MyBucketServerApplication.java
@@ -0,0 +1,13 @@
+package com.junam.my_bucket_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBucketServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBucketServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/junam/my_bucket_server/MyBucketServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junam.my_bucket_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBucketServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
