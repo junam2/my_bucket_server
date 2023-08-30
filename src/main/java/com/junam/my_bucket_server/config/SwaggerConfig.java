8b19a77a1ad8a0a001dfd07f43377cebcca3dd8c
junam2 <speedlin4584@gmail.com>
2023-08-31 00:00:17 +0900

    swagger 설정

---
+++ src/main/java/com/junam/my_bucket_server/config/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package com.junam.my_bucket_server.config;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("My Bucket List")
+                .description("My Bucket List API")
+                .version("1.0.0");
+    }
+}
--- src/main/java/com/junam/my_bucket_server/controller/TestController.java
+++ src/main/java/com/junam/my_bucket_server/controller/TestController.java
@@ -1,12 +1,16 @@
 package com.junam.my_bucket_server.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name= "TEST API")
 @RestController
+@RequestMapping("/api")
 public class TestController {
 
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String hello() {
         return "hello";
     }
